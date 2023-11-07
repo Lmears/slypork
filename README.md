11ea6095ebe70f8973497badd78125956cf1c9f9
Lmears <Lmears@users.noreply.github.com>
2023-11-07 23:36:38 +0000

    deploy: a3f60cf949ab5676630e27b182dd0514f8f4952e

---
+++ README.md
@@ -0,0 +1,2 @@
+# slypork.studio
+Portfolio site featuring my audio mastering, music projects, and software. Connect with my work, listen to my creations, and explore my code through this interactive hub.
---
+++ script.js
@@ -0,0 +1,57 @@
+// Home image animation
+var homeLink = document.getElementById('homeLink');
+var homeLogo = document.querySelector('.home-logo');
+
+var isHovering = false;
+
+homeLink.addEventListener('mouseover', function () {
+    homeLogo.src = 'assets/images/home-hover.png';
+    isHovering = true;
+});
+
+homeLink.addEventListener('mouseout', function () {
+    homeLogo.src = 'assets/images/home.png';
+    isHovering = false;
+});
+
+homeLink.addEventListener('mousedown', function () {
+    homeLogo.src = 'assets/images/home.png';
+});
+
+homeLink.addEventListener('mouseup', function () {
+    if (isHovering) {
+        homeLogo.src = 'assets/images/home-hover.png';
+    }
+});
+
+
+
+// Lightbox modal
+var modal = document.getElementById("myModal");
+var img = document.querySelector('.modal-trigger');
+var modalImg = document.getElementById("img01");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.onkeydown = function (event) {
+    if (event.key === "Escape") {
+        if (modal.style.display === "block") {
+            modal.style.display = "none";
+        }
+    }
+}
