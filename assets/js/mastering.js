70ba4d113b43ad8e153e2e4bd0fabc03969b321e
Lmears <lmears97@gmail.com>
2024-05-27 18:40:03 +1200

    made mastering links clickable

--- assets/js/mastering.js
+++ assets/js/mastering.js
@@ -2,28 +2,182 @@
 var linkColor = '63b2cc';
 
 var links = [
-    { id: 'album=4108014993', name: 'Nomali - Dubcore', credits: 'Credits: Mastering', date: '2024-05-03', type: 'bandcamp' },
-    { id: 'spotify:track:32GAT5y3WYNpvBHAaRk8XU', name: 'bobby mayo - Axolotl', credits: 'Credits: Mastering', date: '2024-03-08', type: 'spotify' },
-    { id: 'album=3993366517', name: 'Teeth and Tail - Air [TNT001]', credits: 'Credits: Mastering', date: '2024-03-01', type: 'bandcamp', isVariousArtists: true },
-    { id: 'playlists/1754058492', name: 'Zero Plussed - Zero Plussed plates', credits: 'Credits: Mastering', date: '2024-02-10', type: 'soundcloud' },
-    { id: 'album=4074789862', name: 'Lü Ka - Burning Bedroom', credits: 'Credits: Mastering', date: '2023-12-08', type: 'bandcamp' },
-    { id: 'spotify:track:5GBzndCuV6kqYZQm3DY78x', name: 'bobby mayo - Funk It Up', credits: 'Credits: Mastering', date: '2023-12-01', type: 'spotify' },
-    { id: 'album=1188484858', name: 'ACOLDPLACE - ACP 2.1', credits: 'Credits: Contributor, Mastering', date: '2023-12-01', type: 'bandcamp', isVariousArtists: true },
-    { id: 'album=79054116', name: 'xtopher - FOR PSY-IENCE', credits: 'Credits: Mastering', date: '2023-11-29', type: 'bandcamp' },
-    { id: 'playlists/1717724526', name: 'Sound Salad - Lettuce Begin', credits: 'Credits: Mastering', date: '2023-11-03', type: 'soundcloud' },
-    { id: 'spotify:track:45qXRq6fkLxcA8i7N7PnDO', name: 'bobby mayo - Dance Attack!', credits: 'Credits: Mastering', date: '2023-09-29', type: 'spotify' },
-    { id: 'tracks/1603868022', name: 'LŪNA - Funk', credits: 'Credits: Mastering', date: '2023-08-30', type: 'soundcloud' },
-    { id: 'album=1182416007', name: 'Khz Collective - Various Artists: Vol. 2', credits: 'Credits: Mastering', date: '2023-08-25', type: 'bandcamp', isVariousArtists: true },
-    { id: 'album=1916604811', name: 'Sauin x Root Basis - Above the Abject Tide', credits: 'Credits: Co-Writing, Co-Mixing, Mastering', date: '2023-08-04', type: 'bandcamp' },
-    // { id: 'playlists/1546172593', name: 'Alley - snake oil', credits: 'Credits: Mastering', date: '2023-01-01', type: 'soundcloud' },
-    { id: 'track=864865153', name: 'Sano - Scandi', credits: 'Credits: Mastering', date: '2022-12-24', type: 'bandcamp' },
-    { id: 'album=2085826436', name: 'Keith - In a Crockpot', credits: 'Credits: Writing, Mixing, Mastering', date: '2022-03-04', type: 'bandcamp' },
-    { id: 'tracks/1214910616', name: 'Sauin - Wharariki', credits: 'Credits: Mastering', date: '2023-02-13', type: 'soundcloud' },
-    { id: 'album=2595562505', name: 'Grains - Dualism', credits: 'Credits: Mastering', date: '2021-11-27', type: 'bandcamp' },
-    { id: 'tracks/1165075729', name: 'Sano - Lost Some Of Me', credits: 'Credits: Mastering', date: '2021-11-23', type: 'soundcloud' },
-    { id: 'tracks/1164338554', name: 'PLISSKIN - Turn And Twist', credits: 'Credits: Mastering', date: '2021-11-21', type: 'soundcloud' },
-    { id: 'album=343771191', name: 'DESTROY WITH SCIENCE - A New View', credits: 'Credits: Mastering', date: '2021-03-12', type: 'bandcamp' },
-    { id: 'album=1891136990', name: 'Grains - ζ', credits: 'Credits: Mastering', date: '2020-09-19', type: 'bandcamp' },
+    {
+        name: 'Nomali - Dubcore',
+        credits: 'Credits: Mastering',
+        date: '2024-05-03',
+        id: 'album=4108014993',
+        url: 'https://post-modernmusic.bandcamp.com/album/dubcore',
+        type: 'bandcamp',
+    },
+    {
+        name: 'bobby mayo - Axolotl',
+        credits: 'Credits: Mastering',
+        date: '2024-03-08', id: 'spotify:track:32GAT5y3WYNpvBHAaRk8XU',
+        url: 'https://open.spotify.com/track/32GAT5y3WYNpvBHAaRk8XU?si=799e38d2744e410c',
+        type: 'spotify',
+    },
+    {
+        name: 'Teeth and Tail - Air [TNT001]',
+        credits: 'Credits: Mastering',
+        date: '2024-03-01',
+        id: 'album=3993366517',
+        url: 'https://teethandtail.bandcamp.com/album/air-tnt001',
+        type: 'bandcamp', isVariousArtists: true,
+    },
+    {
+        name: 'Zero Plussed - Zero Plussed plates',
+        credits: 'Credits: Mastering',
+        date: '2024-02-10',
+        id: 'playlists/1754058492',
+        url: 'https://soundcloud.com/commander_zeroplussed/sets/zero-plussed-plates',
+        type: 'soundcloud',
+    },
+    {
+        name: 'Lü Ka - Burning Bedroom',
+        credits: 'Credits: Mastering',
+        date: '2023-12-08',
+        id: 'album=4074789862',
+        url: 'https://lukaplays.bandcamp.com/album/burning-bedroom',
+        type: 'bandcamp',
+    },
+    {
+        name: 'bobby mayo - Funk It Up',
+        credits: 'Credits: Mastering',
+        date: '2023-12-01',
+        id: 'spotify:track:5GBzndCuV6kqYZQm3DY78x',
+        url: 'https://open.spotify.com/track/5GBzndCuV6kqYZQm3DY78x?si=c8807b5c19734774',
+        type: 'spotify',
+    },
+    {
+        name: 'ACOLDPLACE - ACP 2.1',
+        credits: 'Credits: Contributor, Mastering',
+        date: '2023-12-01',
+        id: 'album=1188484858',
+        url: 'https://acoldplace.bandcamp.com/album/acp-21',
+        type: 'bandcamp',
+        isVariousArtists: true,
+    },
+    {
+        name: 'xtopher - FOR PSY-IENCE',
+        credits: 'Credits: Mastering',
+        date: '2023-11-29',
+        id: 'album=79054116',
+        url: 'https://xtopher69.bandcamp.com/album/for-psy-ience-ep',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Sound Salad - Lettuce Begin',
+        credits: 'Credits: Mastering',
+        date: '2023-11-03',
+        id: 'playlists/1717724526',
+        url: 'https://soundcloud.com/sound-salad-225675916/sets/lettuce-begin-ep',
+        type: 'soundcloud',
+    },
+    {
+        name: 'bobby mayo - Dance Attack!',
+        credits: 'Credits: Mastering',
+        date: '2023-09-29',
+        id: 'spotify:track:45qXRq6fkLxcA8i7N7PnDO',
+        url: 'https://open.spotify.com/track/45qXRq6fkLxcA8i7N7PnDO?si=2dacfa4254eb4948',
+        type: 'spotify',
+    },
+    {
+        name: 'LŪNA - Funk',
+        credits: 'Credits: Mastering',
+        date: '2023-08-30',
+        id: 'tracks/1603868022',
+        url: 'https://soundcloud.com/lunanz/funk',
+        type: 'soundcloud',
+    },
+    {
+        name: 'Khz Collective - Various Artists: Vol. 2',
+        credits: 'Credits: Mastering',
+        date: '2023-08-25',
+        id: 'album=1182416007',
+        url: 'https://khzcollective.bandcamp.com/album/various-artists-vol-2',
+        type: 'bandcamp',
+        isVariousArtists: true,
+    },
+    {
+        name: 'Sauin x Root Basis - Above the Abject Tide',
+        credits: 'Credits: Co-Writing, Co-Mixing, Mastering',
+        date: '2023-08-04',
+        id: 'album=1916604811',
+        url: 'https://teethandtail.bandcamp.com/album/above-the-abject-tide-sauin-x-root-basis',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Sauin - Wharariki',
+        credits: 'Credits: Mastering',
+        date: '2023-02-13',
+        id: 'tracks/1214910616',
+        url: 'https://soundcloud.com/birkenthot/wharariki',
+        type: 'soundcloud',
+    },
+    // {
+    //     id: 'playlists/1546172593',
+    //     name: 'Alley - snake oil',
+    //     credits: 'Credits: Mastering',
+    //     date: '2023-01-01',
+    //     type: 'soundcloud'
+    // },
+    {
+        name: 'Sano - Scandi',
+        credits: 'Credits: Mastering',
+        date: '2022-12-24',
+        id: 'track=864865153',
+        url: 'https://khzcollective.bandcamp.com/track/scandi',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Keith - In a Crockpot',
+        credits: 'Credits: Writing, Mixing, Mastering',
+        date: '2022-03-04',
+        id: 'album=2085826436',
+        url: 'https://keithunsheathed.bandcamp.com/album/in-a-crockpot',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Grains - Dualism',
+        credits: 'Credits: Mastering',
+        date: '2021-11-27',
+        id: 'album=2595562505',
+        url: 'https://grainsnz.bandcamp.com/album/dualism',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Sano - Lost Some Of Me',
+        credits: 'Credits: Mastering',
+        date: '2021-11-23',
+        id: 'tracks/1165075729',
+        url: 'https://soundcloud.com/sano_dng/lost-some-of-me',
+        type: 'soundcloud',
+    },
+    {
+        name: 'PLISSKIN - Turn And Twist',
+        credits: 'Credits: Mastering',
+        date: '2021-11-21',
+        id: 'tracks/1164338554',
+        url: 'https://soundcloud.com/mmcrotn/turn-and-twist',
+        type: 'soundcloud',
+    },
+    {
+        name: 'DESTROY WITH SCIENCE - A New View',
+        credits: 'Credits: Mastering',
+        date: '2021-03-12',
+        id: 'album=343771191',
+        url: 'https://destroywithscience.bandcamp.com/album/a-new-view',
+        type: 'bandcamp',
+    },
+    {
+        name: 'Grains - ζ',
+        credits: 'Credits: Mastering',
+        date: '2020-09-19',
+        id: 'album=1891136990',
+        url: 'https://grainsnz.bandcamp.com/album/-',
+        type: 'bandcamp',
+    },
 ];
 
 // Sort links by date in descending order
@@ -43,8 +197,12 @@ function createIframe(src, allowAutoplay = false) {
 }
 
 // Create an element with text content and class name
-function createElementWithText(tag, text, className) {
+function createElementWithText(tag, text, className, url) {
     var element = document.createElement(tag);
+    if (url != null) {
+        element.href = url;
+        element.target = '_blank';
+    }
     element.textContent = text;
     element.className = className;
     return element;
@@ -63,7 +221,7 @@ function createEmbedElements(item, iframeSrc, allowAutoplay = false) {
 
     wrapperDiv.appendChild(iframeDiv);
 
-    var nameDiv = createElementWithText('div', item.name, 'release-name');
+    var nameDiv = createElementWithText('a', item.name, 'release-name subtle-underline', item.url);
     if (item.isVariousArtists) {
         var variousArtistsSpan = createElementWithText('span', ' (Various Artists)', 'various-artists');
         nameDiv.appendChild(variousArtistsSpan);
