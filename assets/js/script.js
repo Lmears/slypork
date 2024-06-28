8d16104c95cb81a17287c8ec0e9d2b699b17729e
Lmears <Lmears@users.noreply.github.com>
2024-06-28 07:59:01 +0000

    deploy: 98998ded986655ccfbdfa5a9d3b1ad41e3695ea2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ function getLogoPath(file) {
     return baseURL + file;
 }
 
+// Easing function
+function easeInOutElastic(x) {
+    const c5 = (2 * Math.PI) / 4.5;
+    return x === 0 ? 0
+        : x === 1 ? 1
+            : x < 0.5 ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+                : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+}
+
 // Home image animation
 var homeLink = document.getElementById('homeLink');
 var homeLogo = document.querySelector('.home-logo');
