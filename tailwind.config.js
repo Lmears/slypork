a63c125b51f460238f4f2cbf7bfdb7d7dab53141
Lmears <Lmears@users.noreply.github.com>
2025-05-11 07:45:23 +0000

    deploy: 6935807b37cb7691d1ff92f08b6e519cd443fdb9

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',         // Scans HTML files in the root
+    './*/**/*.html',    // Scans HTML files in immediate subdirectories (like cv/index.html)
+    './assets/js/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#073b4c',
+        heading: '#193a4a',
+        body: '#333333',
+        light: '#f3f5f6',
+        section: '#e7ebed',
+      },
+      screens: {
+        'nav-hide': '642px', // Even though this is close to sm: there may be more nav items in the future
+      },
+    }
+  },
+  plugins: [],
+}
